3369f0939c7b898e2dabc4ace97fb06bd5b38888
Shankar Krishnamurthy <krishnamurthyshankar9@gmail.com>
2025-02-09 22:35:08 -0600

    Batch Upload for Viewing Online
    This is just the upload of my first attempt to make a CrewAI crew locally to GitHub for others to view my work.

---
+++ src/testone/main.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+import sys
+import warnings
+
+from datetime import datetime
+
+from testone.crew import Testone
+
+warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
+
+# This main file is intended to be a way for you to run your
+# crew locally, so refrain from adding unnecessary logic into this file.
+# Replace with inputs you want to test with, it will automatically
+# interpolate any tasks and agents information
+
+topic = input("Please enter the topic your want to know more about: ")
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': topic,
+        'current_year': str(datetime.now().year)
+    }
+    
+    try:
+        Testone().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        Testone().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        Testone().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        Testone().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while testing the crew: {e}")
